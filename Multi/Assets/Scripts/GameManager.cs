032dfe0253c128a936facc19dd3a3c481ba36e49
Ben Yi <bencyi@me.com>
2019-08-08 15:58:06 -0400

    Multiplayer broken

--- Multi/Assets/Scripts/GameManager.cs
+++ Multi/Assets/Scripts/GameManager.cs
@@ -63,8 +63,7 @@ namespace Com.MyCompany.MyGame {
             if (!PhotonNetwork.IsMasterClient) {
                 Debug.LogError ("PhotonNetwork : Trying to Load a level but we are not the master Client");
             }
-            Debug.LogFormat ("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
-            PhotonNetwork.LoadLevel ("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel ("Game");
         }
 
         #endregion
