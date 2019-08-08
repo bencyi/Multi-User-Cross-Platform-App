fbbfec6091c1a915b4f89b43fe65f0c523e54aae
Ben Yi <bencyi@me.com>
2019-08-08 00:24:05 -0400

    Initial Game
    Added an environment, a player with first person capabilities.

---
+++ Multi/Assets/characterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+
+    public float speed = 10.0F;
+
+    //public GameObject explosionEffect;
+
+    //public AudioClip explosionClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    /*
+     
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Bomb"))
+        {
+            AudioSource.PlayClipAtPoint(explosionClip , transform.position);
+
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+
+            Destroy(other.gameObject);
+        }
+    }    
+    */
+}
---
+++ Multi/Assets/mouseLook.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour {
+
+//Keeps track of how much movement camera has made, total movement
+    Vector2 mouseLookVar;
+    
+    //Smooths movement of mouse
+    Vector2 smoothV;
+
+    //Mouse sensitivity - movement required of mouse
+    public float sensitivity = 5.0f;
+
+    //Smooths movement
+    public float smoothing = 2.0f;
+
+    //Character that mouse controls
+    GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets change of mouse movement since last update (mousse delta)
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        //Multiplied by sens and smoothing
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        //Moves smoothly between two points, removes snappiness
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f/smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f/smoothing);
+
+        //Movement and smoothing of mouse
+        mouseLookVar += smoothV;
+
+        mouseLookVar.y = Mathf.Clamp(mouseLookVar.y, -90f, 90f);
+
+        //Movement up and down rotated around vector3.right
+        transform.localRotation = Quaternion.AngleAxis(-mouseLookVar.y, Vector3.right);
+
+        //Movement left and right rotated around y
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLookVar.x, character.transform.up);
+        
+    }
+}
